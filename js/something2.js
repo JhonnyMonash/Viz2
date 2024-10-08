aad1b86e2f70873cb8ef7472ed0e424a78795563
john Zhang <jzha0350@student.monash.edu>
2024-10-08 13:16:03 +1100

    Migration

---
+++ js/something2.js
@@ -0,0 +1,64 @@
+var vg_2 = "js/station_patronage.vg.json";
+vegaEmbed("#vg_2", vg_2, { "actions": false }).then(function (result) {
+    var view = result.view;
+    var lines = [
+        'Alamein',
+        'Belgrave',
+        'City Loop',
+        'Craigieburn',
+        'Cranbourne',
+        'Frankston',
+        'Glen Waverley',
+        'Hurstbridge',
+        'Lilydale',
+        'Mernda',
+        'Pakenham',
+        'Sandringham',
+        'Sunbury',
+        'Upfield',
+        'Werribee',
+        'Williamstown'
+    ];
+
+    // Bind checkboxes to Vega-Lite parameters
+    lines.forEach(function (line) {
+        var paramName = 'line_' + line.replace(/\s+/g, '_');
+        var checkbox = document.getElementById(paramName);
+        checkbox.addEventListener('change', function () {
+            view.signal(paramName, checkbox.checked).run();
+        });
+    });
+}).catch(console.error);
+ 
+var vg_1 = "js/test.vg.json";
+vegaEmbed("#vg_1", vg_1, { "actions": false }).then(function (result) {
+    var view = result.view;
+    var lines = [
+        'Alamein',
+        'Belgrave',
+        'City Loop',
+        'Craigieburn',
+        'Cranbourne',
+        'Frankston',
+        'Glen Waverley',
+        'Hurstbridge',
+        'Lilydale',
+        'Mernda',
+        'Pakenham',
+        'Sandringham',
+        'Sunbury',
+        'Upfield',
+        'Werribee',
+        'Williamstown'
+    ];
+
+    // Bind checkboxes to Vega-Lite parameters
+    lines.forEach(function (line) {
+        var paramName = 'line_' + line.replace(/\s+/g, '_');
+        var checkbox = document.getElementById(paramName);
+        checkbox.addEventListener('change', function () {
+            view.signal(paramName, checkbox.checked).run();
+        });
+    });
+}).catch(console.error);
+
