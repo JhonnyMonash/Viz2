39c8c530f05c68ee70757231396e56d34b528ef0
john Zhang <jzha0350@student.monash.edu>
2024-10-08 16:31:56 +1100

    for critic

--- js/viz2.js
+++ js/viz2.js
@@ -1,5 +1,5 @@
 var vg_3 = "js/annual.vg.json";
-vegaEmbed("#vg_2", vg_2, { "actions": false }).then(function (result) { }).catch(console.error);
+vegaEmbed("#vg_3", vg_3, { "actions": false }).then(function (result) { }).catch(console.error);
 
 var vg_2 = "js/station_patronage.vg.json";
 vegaEmbed("#vg_2", vg_2, { "actions": false }).then(function (result) {
